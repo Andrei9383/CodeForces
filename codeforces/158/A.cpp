dfe7d635cb2880920a5e182cbce257ea6a49ad59
Andrei9383 <andreistan275@gmail.com>
2021-09-21 19:03:00 +0300

    Add solution for problem `A - Next Round` Link: https://codeforces.com/contest/158/problem/A Tags: *special problem, implementation, *800 Ref: https://codeforces.com/contest/158/submission/129513609

---
+++ codeforces/158/A.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define ull unsigned long long
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+
+void solve() {
+    ull int n, k, v[100000], a = 0; cin >> n >> k;
+    for(int i = 1; i<=n; i++){
+        cin >> v[i];
+    }
+    for(int i = 1; i<=n; i++){
+        if(v[i] >= v[k] && v[i] > 0){
+            a++;
+        }
+    }
+    cout << a;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    //cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        //cout << endl << "Case #" << t << ": ";
+        solve();
+    }
+    cout << endl;
+}
