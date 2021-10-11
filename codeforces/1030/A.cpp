1bd431d5a450e5239193518e3d41abb80078cf9e
Andrei9383 <andreistan275@gmail.com>
2021-10-11 16:18:00 +0300

    Add solution for problem `A - In Search of an Easy Problem` Link: https://codeforces.com/contest/1030/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1030/submission/131554504

---
+++ codeforces/1030/A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    for(int i = 0; i<n; i++){
+        int x; cin >> x;
+        if(x == 1){
+            cout << "HARD";
+            return 0;
+        }
+    }
+    cout << "EASY";
+    return 0;
+}
